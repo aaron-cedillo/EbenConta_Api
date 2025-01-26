05ec65f6ac30b51dd7d9d65de00cafbfac012d48
Aaron Cedillo <aaroncescalante@gmail.com>
2025-01-26 00:26:20 -0500

    corrección de cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Ruta para obtener todos los usuarios
-router.get('/', userController.getUsers); // No es necesario añadir '/users' aquí
+router.get('/', userController.getUsers); 
 
 module.exports = router;
