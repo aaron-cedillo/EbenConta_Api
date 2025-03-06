51db3e5e2299ae12e0d5534a1044e942dcd21de8
Aaron Cedillo <aaroncescalante@gmail.com>
2025-03-05 19:30:45 -0500

    funcion de ingresos egresos

--- controllers/ingresosegresosController.js
+++ controllers/ingresosegresosController.js
@@ -1,12 +1,12 @@
 const { sql } = require("../config/db");
 
-// Función para obtener el resumen de facturas de un cliente
+// Obtener el resumen de facturas de un cliente con filtro de fecha
 const obtenerFacturasPorCliente = async (req, res) => {
     try {
         const { ClienteID } = req.params;
         const { fechaInicio, fechaFin } = req.query;
 
-        let query = `SELECT * FROM Facturas WHERE ClienteID = @ClienteID`;
+        let query = `SELECT Fecha, Total, Tipo FROM Facturas WHERE ClienteID = @ClienteID`;
 
         const request = new sql.Request();
         request.input("ClienteID", sql.Int, ClienteID);
@@ -25,34 +25,6 @@ const obtenerFacturasPorCliente = async (req, res) => {
     }
 };
 
-const obtenerFacturaPorID = async (req, res) => {
-    try {
-        const { FacturaID } = req.params;
-
-        const facturaIDInt = parseInt(FacturaID, 10);
-        if (isNaN(facturaIDInt)) {
-            return res.status(400).json({ error: "FacturaID debe ser un número válido" });
-        }
-
-        const query = `SELECT * FROM Facturas WHERE FacturaID = @FacturaID`;
-
-        const request = new sql.Request();
-        request.input("FacturaID", sql.Int, facturaIDInt);
-
-        const result = await request.query(query);
-
-        if (result.recordset.length === 0) {
-            return res.status(404).json({ error: "Factura no encontrada" });
-        }
-
-        res.json(result.recordset[0]);
-    } catch (error) {
-        console.error("Error al obtener la factura:", error);
-        res.status(500).json({ error: "Error al obtener la factura" });
-    }
-};
-
 module.exports = {
-    obtenerFacturaPorID,
-    obtenerFacturasPorCliente,
+    obtenerFacturasPorCliente
 };
--- routes/ingresosegresosRoutes.js
+++ routes/ingresosegresosRoutes.js
@@ -1,11 +1,9 @@
-// rutas/facturas.js (o el nombre de tu archivo de rutas)
 const express = require("express");
 const router = express.Router();
 const authenticateJWT = require('../middlewares/authenticateJWT');
 const ingresosegresosController = require("../controllers/ingresosegresosController");
 
-// Ruta para obtener el resumen de las facturas de un cliente
-router.get("/cliente/:ClienteID",authenticateJWT, ingresosegresosController.obtenerFacturasPorCliente);
-router.get("/:FacturaID",authenticateJWT, ingresosegresosController.obtenerFacturaPorID);
+// Ruta para obtener el resumen de las facturas de un cliente con filtro de fecha
+router.get("/cliente/:ClienteID", authenticateJWT, ingresosegresosController.obtenerFacturasPorCliente);
 
 module.exports = router;
